d4bf28f527f7738bf177a2e8688847295800a3f0
weih <xiaohaoprog@gmail.com>
2011-11-30 23:31:16 +0800

    add thread

--- proxy_spider.rb
+++ proxy_spider.rb
@@ -12,32 +12,46 @@ PAGE = ARGV
 PROXY_ADDR = '127.0.0.1'
 PROXY_PORT = 8087
 TARGET_HOST = 'sakura99.com'
-TARGET_URI = '/alabout/index.php?do=index&class_id=001&page='.concat PAGE[0]
+TARGET_URI = '/alabout/index.php?do=index&class_id=001&page=17'#.concat PAGE[0]
 IMG_REGEX_SAKURA = %r(<a.*>(http:\/\/.*\.jpg)<\/a>)
 IMG_REGEX_INNER = %r(img\sid="show_image".*src=\"(.*?)\")
 
-res = Net::HTTP.start(TARGET_HOST) do |http|
-  begin
-    http.get(TARGET_URI).body.scan(IMG_REGEX_SAKURA).each do |item|
-      #m = IMG_REGEX_SAKURA.match http.get(TARGET_URI).body
-      #p 'item ' + item[0]
-      #inner_addr = $1
-      #p 'inner_addr ' + inner_addr
-      imgchili_addr = Net::HTTP.get_response(URI.parse(item[0])).body
-      imgchili_m = IMG_REGEX_INNER.match imgchili_addr
-      the_image = $1
-      p 'Done ' + the_image if !the_image.nil?
-      filename = %r(.*\/(.*.jpg)).match the_image
-      File.open($1,  "w+") do |f|
-        f.write(Net::HTTP.get_response(URI.parse(the_image)).body)
-        #Profiler__.print_profile(STDOUT)
-      end
-    end
-  rescue
-    next
+def get_image(image_uri)
+  #p item
+  imgchili_addr = Net::HTTP.get_response(URI.parse(image_uri)).body
+  #open_uri(URI.parse(item)) do |imgchili_addr|
+  #p imgchili_addr
+  imgchili_m = IMG_REGEX_INNER.match imgchili_addr
+  #p imgchili_m
+  the_image = $1
+  filename = %r(.*\/(.*.jpg)).match the_image
+  File.open($1,  "w+") do |f|
+    f.write(Net::HTTP.get_response(URI.parse(the_image)).body)
   end
-    #Profiler__.print_profile(STDOUT)
-  #Net::HTTP.get_response(URI.parse(the_image)).body
 end
+
+all_image = []
+
+res = Net::HTTP.start(TARGET_HOST) do |http|
+  #begin
+    http.get(TARGET_URI).body.scan(IMG_REGEX_SAKURA).each do |item|
+        all_image << item[0]
+    end
+end
+
+all_image.each do |item|
+  Thread.new {
+    get_image(item)
+    p 'Done ' + item if !item.nil?
+    Thread.current.exit
+  }  
+end
+
+loop {
+  p Thread.list
+  break if Thread.list.size == 1
+  sleep 30
+}
+#end
 
 #p res.body
