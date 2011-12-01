b7ff4308667574b94e3038b0973ceeaa9c3558e0
weih <xiaohaoprog@gmail.com>
2011-12-01 22:21:22 +0800

    exit when all thread is done

--- proxy_spider.rb
+++ proxy_spider.rb
@@ -8,50 +8,73 @@ require 'uri'
 #Profiler__.start_profile
 
 PAGE = ARGV
-
 PROXY_ADDR = '127.0.0.1'
 PROXY_PORT = 8087
 TARGET_HOST = 'sakura99.com'
-TARGET_URI = '/alabout/index.php?do=index&class_id=001&page=17'#.concat PAGE[0]
+target_uri = '/alabout/index.php?do=index&class_id=001&page='#.concat PAGE[0]
 IMG_REGEX_SAKURA = %r(<a.*>(http:\/\/.*\.jpg)<\/a>)
 IMG_REGEX_INNER = %r(img\sid="show_image".*src=\"(.*?)\")
+IMAGE_DIR = 'img/'
+
+Thread.abort_on_exception = true
 
 def get_image(image_uri)
   #p item
-  imgchili_addr = Net::HTTP.get_response(URI.parse(image_uri)).body
-  #open_uri(URI.parse(item)) do |imgchili_addr|
-  #p imgchili_addr
-  imgchili_m = IMG_REGEX_INNER.match imgchili_addr
-  #p imgchili_m
-  the_image = $1
-  filename = %r(.*\/(.*.jpg)).match the_image
-  File.open($1,  "w+") do |f|
-    f.write(Net::HTTP.get_response(URI.parse(the_image)).body)
+  begin
+    imgchili_addr = Net::HTTP.get_response(URI.parse(image_uri)).body
+    #open_uri(URI.parse(item)) do |imgchili_addr|
+    #p imgchili_addr
+    imgchili_m = IMG_REGEX_INNER.match imgchili_addr
+    #p imgchili_m
+    the_image = $1
+    filename = %r(.*\/(.*.jpg)).match the_image
+    File.open(IMAGE_DIR + $1,  "w+") do |f|
+      f.write(Net::HTTP.get_response(URI.parse(the_image)).body)
+    end
+  rescue Timeout::Error
+    retry
   end
 end
 
+def join_all
+  main = Thread.main
+  current = Thread.current
+  all = Thread.list
+  all.each { |t| t.join unless t == current or t == main }
+end
+
+# main phrase
+
 all_image = []
 
-res = Net::HTTP.start(TARGET_HOST) do |http|
-  #begin
-    http.get(TARGET_URI).body.scan(IMG_REGEX_SAKURA).each do |item|
-        all_image << item[0]
-    end
+Dir.mkdir('img') if !File.exist?('img/')
+(21..23).each do |iter|
+  res = Net::HTTP.start(TARGET_HOST) do |http|
+    #begin
+      http.get(target_uri + iter.to_s).body.scan(IMG_REGEX_SAKURA).each do |item|
+          all_image << item[0]
+      end
+  end
 end
 
 all_image.each do |item|
   Thread.new {
     get_image(item)
-    p 'Done ' + item if !item.nil?
-    Thread.current.exit
+    #item
+    #p 'Done ' + item if !item.nil?
+    #Thread.current.exit
   }  
 end
 
-loop {
-  p Thread.list
-  break if Thread.list.size == 1
-  sleep 30
-}
+join_all
+
+#loop {
+  #Thread.list.each do |th|
+  #  p th.value
+  #end
+  #break if Thread.list.size == 1
+  #sleep 30
+#}
 #end
 
 #p res.body
