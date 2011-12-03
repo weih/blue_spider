2cb309df590bd25fce41b52e8313abb845111a3d
weih <xiaohaoprog@gmail.com>
2011-12-03 16:48:31 +0800

    check for nil filename

--- proxy_spider.rb
+++ proxy_spider.rb
@@ -7,11 +7,11 @@ require 'uri'
 
 #Profiler__.start_profile
 
-PAGE = ARGV
+#PAGE = ARGV
 PROXY_ADDR = '127.0.0.1'
 PROXY_PORT = 8087
 TARGET_HOST = 'sakura99.com'
-target_uri = '/alabout/index.php?do=index&class_id=001&page='#.concat PAGE[0]
+TARGET_URI = '/alabout/index.php?do=index&class_id=001&page='#.concat PAGE[0]
 IMG_REGEX_SAKURA = %r(<a.*>(http:\/\/.*\.jpg)<\/a>)
 IMG_REGEX_INNER = %r(img\sid="show_image".*src=\"(.*?)\")
 IMAGE_DIR = 'img/'
@@ -27,12 +27,15 @@ def get_image(image_uri)
     imgchili_m = IMG_REGEX_INNER.match imgchili_addr
     #p imgchili_m
     the_image = $1
+    return if the_image.nil?
     filename = %r(.*\/(.*.jpg)).match the_image
     File.open(IMAGE_DIR + $1,  "w+") do |f|
       f.write(Net::HTTP.get_response(URI.parse(the_image)).body)
     end
   rescue Timeout::Error
     retry
+  rescue TypeError
+    #p 'can\'t find the name ' + the_image
   end
 end
 
@@ -44,29 +47,84 @@ def join_all
 end
 
 # main phrase
+def sakura
+  
+  all_image = []
 
-all_image = []
+  Dir.mkdir(IMAGE_DIR) if !File.exist?(IMAGE_DIR)
+  (1..3).each do |iter|
+    res = Net::HTTP.start(TARGET_HOST) do |http|
+      #begin
+        http.get(TARGET_URI + iter.to_s).body.scan(IMG_REGEX_SAKURA).each do |item|
+            all_image << item[0]
+        end
+    end
+  end
+  
+  #p all_image
 
-Dir.mkdir('img') if !File.exist?('img/')
-(21..23).each do |iter|
-  res = Net::HTTP.start(TARGET_HOST) do |http|
-    #begin
-      http.get(target_uri + iter.to_s).body.scan(IMG_REGEX_SAKURA).each do |item|
-          all_image << item[0]
+  all_image.each do |item|
+    Thread.new {
+      get_image(item)
+      #item
+      #p 'Done ' + item if !item.nil?
+      #Thread.current.exit
+    }  
+  end
+
+  join_all
+
+end
+
+sakura
+
+TARGET_HOST_2 = 'www.beautyleg.cc'
+BEAUTY_LEG = '/new/2011-09-12-No-581-Jill-70P'
+BEAUTY_REGEX = %r(.*-(No.*)(\d\d\d?)P)
+BIG_IMAGE_REGEX = %r(<a href="(.*)" class="g-fullsize-link")
+
+def beauty
+  images = []
+  
+  Net::HTTP.start(TARGET_HOST_2) do |http|
+    BEAUTY_REGEX.match(BEAUTY_LEG)
+    photos = $2
+    signal_image_uri = "/Beautyleg-" + $1
+    #p signal_image_uri
+    1.upto(10) do |image_iter|
+      #Thread.new do
+        signal_image_uri_with_index = signal_image_uri + "%04d" % image_iter.to_s
+        BIG_IMAGE_REGEX.match(http.get(BEAUTY_LEG + signal_image_uri_with_index).body)
+        #p $1
+        images << TARGET_HOST_2 + $1
+        #p http.get(BEAUTY_LEG + signal_image_uri_with_index).body
+      #end
+    end
+    
+    images.each_with_index do |iter, n|
+      Thread.new do
+        #p iter
+        begin
+          File.open(n.to_s + '.jpg', 'w') do |f|
+            f.write(Net::HTTP.get_response(URI.parse("http://" + iter)).body)
+          end
+          p n.to_s + 'Done'
+        rescue Timeout::Error
+          retry
+        end
       end
+    end
+    #p images
+    #puts http.get(BEAUTY_LEG + "/Beautyleg-No-594-Jill-0001").body
+    #File.open('img', 'w') do |f|
+    #  f.write(http.get(BEAUTY_LEG).body)
+    #end
   end
 end
 
-all_image.each do |item|
-  Thread.new {
-    get_image(item)
-    #item
-    #p 'Done ' + item if !item.nil?
-    #Thread.current.exit
-  }  
-end
+#beauty
 
-join_all
+#join_all
 
 #loop {
   #Thread.list.each do |th|
